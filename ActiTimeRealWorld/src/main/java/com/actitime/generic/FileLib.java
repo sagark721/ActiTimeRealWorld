5407303782e6716d7fd6c03ae5e01dcc9ce15723
HAREESH <HAREESH@LAPTOP-57TQEETI>
2023-06-07 17:14:03 +0530

    filelib and property data updated

--- ActiTimeRealWorld/src/main/java/com/actitime/generic/FileLib.java
+++ ActiTimeRealWorld/src/main/java/com/actitime/generic/FileLib.java
@@ -1,5 +1,34 @@
 package com.actitime.generic;
 
-public class FileLib {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
-}
+/**
+ * This is a generic class for data driven testing
+ * @author HAREESH
+ *
+ */
+public class FileLib {
+	/**
+	 * This is generic method for reading the data frome the property file
+	 * @param key
+	 * @return String
+	 * @throws IOException
+	 */
+	public String getPropertyData(String key) throws IOException {
+
+		FileInputStream fis=new FileInputStream("./data/propertydata.property");
+
+		Properties p=new Properties();
+
+		p.load(fis);
+
+		String data=p.getProperty(key);
+		
+		return data;
+	}
+
+	
+	}
+
