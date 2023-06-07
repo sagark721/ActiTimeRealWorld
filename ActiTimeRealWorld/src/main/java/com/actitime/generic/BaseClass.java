89f0076cd5e1d7ba09d1cd9d901cc7507eac5936
DELL <DELL@jk>
2023-06-07 16:57:09 +0530

    mm

--- ActiTimeRealWorld/src/main/java/com/actitime/generic/BaseClass.java
+++ ActiTimeRealWorld/src/main/java/com/actitime/generic/BaseClass.java
@@ -1,6 +1,19 @@
 package com.actitime.generic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
 public class BaseClass {
+	public static WebDriver driver;
+	static {
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/driver/chromedriver.exe");
+	}
+	@BeforeTest
+	public void openBrowser() {
+		driver=new ChromeDriver();
+		
+	}
 
 	
 	
